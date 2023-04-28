fe0b3e80a7ea8e3bd4368c18b6319984ee26fb9b
HeavyByte <english.w.andrew@gmail.com>
2023-04-28 10:01:14 -0500

    Test class name and styles

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,18 @@
     var newTag = document.createElement(tag);
     var newContent = document.createTextNode(children);
     newTag.appendChild(newContent);
-    
+
+    if(!!attrs){
+        if(!!attrs.class){
+            newTag.className = attrs.class
+        }   
+    }
+
     return newTag;
   }
 
 
-  var newDiv = h("div", null, "Hello Kindle");
+  var newDiv = h("div", {class: "largePrint"}, "Hello Kindle");
 
   // add the newly created element and its content into the DOM
   const app = document.getElementById("app");
