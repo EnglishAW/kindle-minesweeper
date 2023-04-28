0d4f32721a7a75686c31f5310a9d2cba9e9998f4
HeavyByte <english.w.andrew@gmail.com>
2023-04-28 10:34:34 -0500

    single cell

--- dist/index.js
+++ dist/index.js
@@ -34,9 +34,9 @@ function makeCellArray() {
 
 //   var textP = h("p", {}, ["I'm a paragragh!"])
 //   var textP2 = h("p", {}, ["I'm a paragragh Too!"])
-var cellArray = makeCellArray()
-console.log(cellArray)
-var boardEl = h("div", {class: "board"}, cellArray)
+// var cellArray = makeCellArray();
+// console.log(cellArray)
+var boardEl = h("div", {class: "board"}, [makeCell()]);
 // var newDiv = h("div", {class: "largePrint"}, ["Hello World"]);
 
 // add the newly created element and its content into the DOM
