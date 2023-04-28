2092bd686a6669fd226f1c76cb1adc8ff919f384
HeavyByte <english.w.andrew@gmail.com>
2023-04-28 10:43:21 -0500

    test cells

--- dist/index.js
+++ dist/index.js
@@ -25,18 +25,18 @@ function makeCell() {
     return h("div", {class: "cell"}, []);
 }
 function makeCellArray() {
-    var initArray = new Array(10);
-    // var cellArray = initArray.map(function (){
-    //     return makeCell();
-    // })
-    return initArray;
+    var cellArray = [];
+    for(var i = 0; i < 100; i++){
+        cellArray.push(makeCell());
+    }
+    return cellArray;
 }
 
 //   var textP = h("p", {}, ["I'm a paragragh!"])
 //   var textP2 = h("p", {}, ["I'm a paragragh Too!"])
 var cellArray = makeCellArray();
 // console.log(cellArray)
-var boardEl = h("div", {class: "board"}, [makeCell()]);
+var boardEl = h("div", {class: "board"}, cellArray);
 // var newDiv = h("div", {class: "largePrint"}, ["Hello World"]);
 
 // add the newly created element and its content into the DOM
